28070eb54c298600b50fdb987c9381c709eed690
Leonam Quintão <leonamquintao@gmail.com>
2022-03-30 12:34:00 -0300

    add transpiler

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import * as esbuild from 'esbuild-wasm';
 
 const App: React.FC = () => {
+
+  const esbuildRef = useRef<any>();
+
+  const startService = async () => {
+    esbuildRef.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm'
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  },[]);
+  
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  const transpile = async () => {
+    if(!esbuildRef.current) return;
+    const result = await esbuildRef.current.transform(input, {
+      loader: 'jsx',
+      target: 'es2015',
+    });
+
+    setCode(result.code);
+  };
+
   return (
     <div>
-      <h1>App TSX</h1>
+     <textarea
+      value={input}
+      onChange={(e) => setInput(e.target.value)}>
+     </textarea>
+     <div>
+       <button onClick={transpile}>Submit</button>
+     </div>
+     <pre>{code}</pre>
     </div>
   );
 }
