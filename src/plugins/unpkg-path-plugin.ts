6df86a06c7469c682886433153800bb0e3ffbaba
Leonam Quintão <leonamquintao@gmail.com>
2022-04-04 17:27:04 -0300

    update unpk plugin

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,30 +1,40 @@
 import * as esbuild from 'esbuild-wasm';
- 
+import axios from 'axios';
+
 export const unpkgPathPlugin = () => {
+  const BASE_URL = 'https://unpkg.com';
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResole', args);
-        return { path: args.path, namespace: 'a' };
+        if (args.path === 'index.js') {
+          return { path: args.path, namespace: 'a' };
+        }
+
+        if (args.path.includes('./') || args.path.includes('../')) {
+          return {
+            namespace: 'a',
+            path: new URL(
+              args.path,
+              BASE_URL + args.resolveDir + '/'
+            ).href,
+          };
+        }
+
+        return {
+          namespace: 'a',
+          path: `${BASE_URL}/${args.path}`
+        }
       });
  
       build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log('onLoad', args);
- 
         if (args.path === 'index.js') {
+          const { data, request } = await axios.get(args.path);
           return {
             loader: 'jsx',
-            contents: `
-              import message from './message';
-              console.log(message);
-            `,
-          };
-        } else {
-          return {
-            loader: 'jsx',
-            contents: 'export default "hi there!"',
-          };
+            contents: data,
+            resolveDir: new URL('./', request.responseURL).pathname,
+          }
         }
       });
     },
