105427963bd2e34a8113e316948382e730cb576e
Leonam Quintão <leonamquintao@gmail.com>
2022-04-05 16:05:45 -0300

    refactoring plugin

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -2,31 +2,34 @@ import * as esbuild from 'esbuild-wasm';
 import axios from 'axios';
 import localforage from 'localforage';
 
-
 const fileCache = localforage.createInstance({
   name: 'filecache',
 });
 
-export const unpkgPathPlugin = () => {
+export const unpkgPathPlugin = (inputCode: string) => {
   const BASE_URL = 'https://unpkg.com';
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
+
+      // handle root entry file of 'index.js'
+      build.onResolve({ filter: /(^index\.js$)/}, () => {
+        return { path: 'index.js', namespace: 'a' };
+      });
+
+      // handle relative paths in module
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
+        return {
+          namespace: 'a',
+          path: new URL(
+            args.path,
+            BASE_URL + args.resolveDir + '/'
+          ).href,
+        };
+      });
+
+      // handle main file of module
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        if (args.path === 'index.js') {
-          return { path: args.path, namespace: 'a' };
-        }
-
-        if (args.path.includes('./') || args.path.includes('../')) {
-          return {
-            namespace: 'a',
-            path: new URL(
-              args.path,
-              BASE_URL + args.resolveDir + '/'
-            ).href,
-          };
-        }
-
         return {
           namespace: 'a',
           path: `${BASE_URL}/${args.path}`
@@ -37,10 +40,7 @@ export const unpkgPathPlugin = () => {
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
-            contents: `
-              import react from 'react';
-              console.log(message);
-            `,
+            contents: inputCode,
           };
         }
 
