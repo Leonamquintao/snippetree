cbf0f72a5ddf8ab411d44fc9eaf61dc03be7950b
Leonam Quintão <leonamquintao@gmail.com>
2022-04-07 13:27:29 -0300

    remove esbuild local

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ const App: React.FC = () => {
   const startService = async () => {
     esbuildRef.current = await esbuild.startService({
       worker: true,
-      wasmURL: '/esbuild.wasm'
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
     });
   };
 
